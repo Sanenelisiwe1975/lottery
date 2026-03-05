280ee88117780fc0096940ac445be82eb8b322fe
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-03-05 13:52:03 +0200

    vrf edit

--- contracts/mocks/VRFCoordinatorV2Mock.sol
+++ contracts/mocks/VRFCoordinatorV2Mock.sol
@@ -2,4 +2,4 @@
 // Sourced from Chainlink's official test helpers (simplified for local use).
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
